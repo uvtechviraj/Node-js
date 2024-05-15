41570b9a426848c5df045a79d2d3dc30b189d855
uvtechviraj <161702273+uvtechviraj@users.noreply.github.com>
2024-05-15 13:58:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Welcome to UvTech'); //write a response to the client
+  res.write('Hello world'); //write a response to the client
   res.end(); //end the response
 }).listen(80); //the server object listens on port 80 , mention your port specific
 
